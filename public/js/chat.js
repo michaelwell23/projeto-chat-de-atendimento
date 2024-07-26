e0d2b1eebbe8dd7097f7c51f68c6601c631c19c2
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-07-26 07:48:45 -0300

    Começando com as configurações do  socket.io e configurações do ejs

---
+++ public/js/chat.js
@@ -0,0 +1,5 @@
+document.querySelector('#start_chat').addEventListener('click', (event) => {
+  console.log('Clicou!!');
+});
+
+// PAUSADO EM 26MIN
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { request } from 'express';
+
+import path from 'path';
+
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 import routes from './routes';
 import './database';
@@ -8,7 +13,25 @@ const port = 3000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/pages/client', (request, response) => {
+  return response.render('html/client.html');
+});
+
+const http = createServer(app);
+
+const io = new Server(http);
+io.on('connection', (socket: Socket) => {
+  console.log('Se conectou', socket.id);
+});
+
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+http.listen(port, () =>
+  console.log(`Example app listening on in http://localhost:${port}`)
+);
